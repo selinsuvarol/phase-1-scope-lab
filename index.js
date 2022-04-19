06349fb603869168ec9bdc6429586b178cd74675
Selin Su Varol <selinsuvarol@gmail.com>
2022-04-19 12:36:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'x';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+};
+
+function setBestCustomer(){
+   return bestCustomer= "not bob";
+};  
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+};
+//console.log(overwriteBestCustomer());
+
+function changeLeastFavoriteCustomer() {
+ return leastFavoriteCustomer = "billy";
+};
